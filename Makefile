445ffb71b99c852e2f3a100d41c8b3f1d63321f2
Jarry Shaw <jarryshaw@icloud.com>
2020-12-07 23:05:12 +0800

    added setup.py

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
+.PHONY: dist
+
 export PIPENV_VERBOSITY = -1
 
 isort:
 	pipenv run isort -l100 --color flask_datatables
+
+dist-clean:
+	[ -f dist/*.whl ] && mkdir -p wheels && mv dist/*.whl wheels || true
+	[ -f dist/*.tar.gz ] && mkdir -p sdist && mv dist/*.tar.gz sdist || true
+	[ -f dist/*.egg ] && mkdir -p eggs && mv dist/*.egg eggs || true
+
+dist-build: dist-clean
+	pipenv run python setup.py sdist bdist_wheel
+
+dist:
+	twine check dist/*
+	twine upload --skip-existing dist/*
---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+__version__ = '0.0.0.dev0'
+
+setup(
+    name='Flask-DT-peewee',
+    version=__version__,
+    description='Flask integration with DataTables and peewee.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/JarryShaw/Flask-DataTables',
+    author='Jarry Shaw',
+    author_email='jarryshaw@icloud.com',
+    license='BSD License',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ],
+    keywords='bpc backport utilities',
+    packages=['flask_datatables'],
+    package_data={'flask_datatables': ['py.typed']},
+    python_requires='>=3.6',
+    install_requires=[
+        'Flask',
+        'peewee',
+        'typing;python_version<"3.5"',
+        'typing_extensions',
+    ],
+)
